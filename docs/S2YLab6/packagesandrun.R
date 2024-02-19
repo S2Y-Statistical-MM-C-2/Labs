1f97d05dd3eb6e02b836e97ad8c0c1155134f6e8
xiao-chen-yang <102661850+xiao-chen-yang@users.noreply.github.com>
2024-02-19 15:10:57 +0000

    update

--- docs/S2YLab6/packagesandrun.R
+++
@@ -1,7 +0,0 @@
-library(webexercises)
-library(bookdown)
-library(servr)
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
-preview_chapter("Example1.Rmd")
-bookdown::serve_book(dir = ".", output_dir = "_book", preview = TRUE,
-          in_session = TRUE, quiet = FALSE)
