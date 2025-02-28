7737fa165f76f920e0439eeb52be91ef0939b0ef
xiao-chen-yang <102661850+xiao-chen-yang@users.noreply.github.com>
2025-02-28 14:30:31 +0000

    update

--- docs/S2YLab8/R/webex.R
+++ docs/S2YLab8/R/webex.R
@@ -1,1 +1,15 @@
-library(webexercises)
+suppressPackageStartupMessages({
+  library(webexercises)
+})
+
+knitr::knit_hooks$set(webex.hide = function(before, options, envir) {
+  if (before) {
+    if (is.character(options$webex.hide)) {
+      hide(options$webex.hide)
+    } else {
+      hide()
+    }
+  } else {
+    unhide()
+  }
+})
